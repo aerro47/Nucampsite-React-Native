870100f053546693f02481c6bcc6f2a1d84c244b
Tarik Joullanar <aerro47@gmail.com>
2021-01-02 13:12:23 -0600

    Weekly Assignment 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 
+const AboutNavigator = createStackNavigator(
+    {
+        About: {screen: About}
+    }, 
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: {screen: Contact}
+    }, 
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
@@ -46,7 +81,10 @@ const HomeNavigator = createStackNavigator(
 const MainNavigator = createDrawerNavigator(
     {
       Home: { screen: HomeNavigator },
-      Directory: { screen: DirectoryNavigator} 
+      Directory: { screen: DirectoryNavigator},
+      About: { screen: AboutNavigator},
+      Contact: { screen: ContactNavigator}
+
     },
     {
         drawerBackgroundColor: '#CEC8FF'
