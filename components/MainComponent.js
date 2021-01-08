40570de32a7db9d487641d084b421e3a6adb54a9
Tarik Joullanar <aerro47@gmail.com>
2021-01-08 13:58:18 -0600

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import Contact from './ContactComponent';
+import Reservation from './ReservationComponent';
 import About from './AboutComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
@@ -66,6 +67,30 @@ const ContactNavigator = createStackNavigator(
         })
     }
 );
+
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: {screen: Reservation}
+    }, 
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+            name='tree'
+            type='font-awesome'
+            iconStyle={styles.stackIcon}
+            onPress={() => navigation.toggleDrawer()}
+        />
+        })
+    }
+);
+
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { 
@@ -162,6 +187,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                     name='list'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+                )
+            }
+        },
+        Reservation: { 
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                    name='tree'
                     type='font-awesome'
                     size={24}
                     color={tintColor}
